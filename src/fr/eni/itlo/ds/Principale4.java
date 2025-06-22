db0e986c0e89508fcf027fd852b44dc05580123e
Dominique SAUVIGNON <dominique.sauvignon@outlook.fr>
2025-06-22 10:46:13 +0200

    Fin de 4.11 - Démonstration : Les Streams

--- src/fr/eni/itlo/ds/Principale4.java
+++ src/fr/eni/itlo/ds/Principale4.java
@@ -2,6 +2,7 @@ package fr.eni.itlo.ds;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import fr.eni.itlo.ds.parc.Moto;
 import fr.eni.itlo.ds.parc.Vehicule;
@@ -18,10 +19,19 @@ public class Principale4 {
 		parc.add(new Velo(12, 50, "VTT"));
 		parc.add(new Moto(120, 250, 1400));
 		
+		/*
 		System.out.println("Taille du parc : " + parc.size());
 		
 		Vehicule veh = parc.get(1);
 		System.out.println("Vitesse : " + veh.getVitesse());
+		*/
+		
+		
+		// 4.44 : re créer grâce au stram une collection qui ne contient que les véhicule rapides : vitesse max > 150
+		List<Vehicule> vehiculesRapides = parc.stream()
+			.filter(v -> v.getVitesseMaximum() > 150 )
+			.collect(Collectors.toList());
+		
+		System.out.println("Nombre de véhicules rapide dans cette collection : " + vehiculesRapides.size());
 	}
-
 }
