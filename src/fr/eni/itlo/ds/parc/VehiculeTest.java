1806dd8ac8a07439bc485f2920c9e658e2f1890e
Dominique SAUVIGNON <dominique.sauvignon@outlook.fr>
2025-07-04 13:45:34 +0200

    Fin de 06.12 - Démonstration - JUnit

---
+++ src/fr/eni/itlo/ds/parc/VehiculeTest.java
@@ -0,0 +1,40 @@
+package fr.eni.itlo.ds.parc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VehiculeTest {
+
+	@Test
+	void testVehicule() {
+		Vehicule veh = new Vehicule();
+		assertNotNull(veh);
+	}
+
+	@Test
+	void testVehiculeIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAccelerer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFreiner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVitesse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVitesseMaximum() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,5 @@
 module fr.eni.itlo.ds{
 	exports fr.eni.itlo.ds;
 	requires org.apache.logging.log4j;
+	requires org.junit.jupiter.api;
 }
